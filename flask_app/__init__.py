08160d704184abacc58aa7fe506abf5584aba24f
Charan Karthik <secharank@gmail.com>
2023-01-30 15:10:46 -0800

    put secret key in .env

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,11 @@
 # print("running init file")
 
 from flask import Flask
+import os
+from dotenv import load_dotenv
+
+
 app = Flask(__name__)
-app.secret_key = "St0nks!" # Needed for session
+
+load_dotenv()
+app.secret_key = os.getenv("APP_SECRET_KEY") # Needed for session
